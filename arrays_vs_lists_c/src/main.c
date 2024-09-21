bfe6faaf6175b96fef257f87f7e58f99599037f8
seandavidreed <seanreed7992@gmail.com>
2024-09-21 11:25:46 -0700

    Refactor code: consolidate test and write to file functions

--- arrays_vs_lists_c/src/main.c
+++ arrays_vs_lists_c/src/main.c
@@ -10,30 +10,16 @@
 
 double test_list(unsigned n, unsigned insert_idx);
 double test_array(unsigned n, unsigned insert_idx);
-void test_changing_n(int cycles, unsigned batches_n[], double list_runtimes[], double array_runtimes[]);
-void test_changing_insert(double list_runtimes[], double array_runtimes[]);
-void save_changing_n_results(int cycles, unsigned batches_n[], double list_runtimes[], double array_runtimes[]);
-void save_changing_insert_results(double list_runtimes[], double array_runtimes[]);
+void test_changing_n(int cycles);
+void test_changing_insert();
 
 int main(int argc, char* argv[]) {
+
 	if (!strcmp(argv[1], "0")) {
-		int cycles = 100;
-		double array_runtimes[cycles];
-		double list_runtimes[cycles];
-		unsigned batches_n[cycles];
-		// Initialize batches
-		batches_n[0] = 1000000;
-		for (int i = 1; i < cycles; i++) {
-			batches_n[i] = batches_n[i - 1] + 100000;
-		}
-		test_changing_n(cycles, batches_n, list_runtimes, array_runtimes);
-		save_changing_n_results(cycles, batches_n, list_runtimes, array_runtimes);
+		test_changing_n(100);
 	}
 	else {
-		double list_runtimes[1000];
-		double array_runtimes[1000];
-		test_changing_insert(list_runtimes, array_runtimes);
-		save_changing_insert_results(list_runtimes, array_runtimes);
+		test_changing_insert();
 	}
 
 	return 0;
@@ -73,7 +59,17 @@ double test_array(unsigned n, unsigned insert_idx) {
 	return (double) cycles / CLOCKS_PER_SEC;
 }
 
-void test_changing_n(int cycles, unsigned batches_n[], double list_runtimes[], double array_runtimes[]) {
+void test_changing_n(int cycles) {
+	// Declare variables
+	double array_runtimes[cycles];
+	double list_runtimes[cycles];
+	unsigned batches_n[cycles];
+	// Initialize batches
+	batches_n[0] = 1000000;
+	for (int i = 1; i < cycles; i++) {
+		batches_n[i] = batches_n[i - 1] + 100000;
+	}
+
 	// Run cycles and collect runtimes
 	double runtime = 0.0;
 	for (int i = 0; i < cycles; i++) {
@@ -86,23 +82,8 @@ void test_changing_n(int cycles, unsigned batches_n[], double list_runtimes[], d
 		printf("%f\n", runtime);
 		array_runtimes[i] = runtime;
 	}
-}
 
-void test_changing_insert(double list_runtimes2[], double array_runtimes2[]) {
-	double runtime = 0.0;
-	
-	for (unsigned i = 1; i < 1000; i++) {
-		runtime = test_list(100000, i*100 - 1);
-		printf("Cycle: %u --- Insertion Index: %u --- Runtimes: %f --- ", i, i*100 - 1, runtime);
-		list_runtimes2[i] = runtime;
-		
-		runtime = test_array(100000, i* 100 - 1);
-		printf("%f\n", runtime);
-		array_runtimes2[i] = runtime;
-	}
-}
-
-void save_changing_n_results(int cycles, unsigned batches_n[], double list_runtimes[], double array_runtimes[]) {
+	// Save results to file
 	FILE* fp = fopen("data/test1_results.csv", "w");
 	char result[20000] = {};
 
@@ -117,14 +98,30 @@ void save_changing_n_results(int cycles, unsigned batches_n[], double list_runti
 	fclose(fp);
 }
 
-void save_changing_insert_results(double list_runtimes[], double array_runtimes[]) {
+void test_changing_insert() {
+	int samples = 1000;
+	double runtime = 0.0;
+	double list_runtimes[samples];
+	double array_runtimes[samples];
+	
+	for (unsigned i = 1; i < samples; i++) {
+		runtime = test_list(100000, i*100 - 1);
+		printf("Cycle: %u --- Insertion Index: %u --- Runtimes: %f --- ", i, i*100 - 1, runtime);
+		list_runtimes[i] = runtime;
+		
+		runtime = test_array(100000, i* 100 - 1);
+		printf("%f\n", runtime);
+		array_runtimes[i] = runtime;
+	}
+
+	// Save results to file
 	FILE* fp = fopen("data/test2_results.csv", "w");
 	char result[100000] = {};
 
 	strcat(result, "Insertion Index,List,Array\n");
-	for (unsigned i = 0; i < 1000; i++) {
+	for (unsigned i = 0; i < samples; i++) {
 		char buffer[100] = {};
-		sprintf(buffer, "%u,%f,%f\n", i, list_runtimes[i], array_runtimes[i]);
+		sprintf(buffer, "%u,%f,%f\n", i*100, list_runtimes[i], array_runtimes[i]);
 		strcat(result, buffer);
 	}
 
